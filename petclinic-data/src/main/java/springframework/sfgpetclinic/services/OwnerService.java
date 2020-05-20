519646c9518fe356c30c2fe139f335c6f213242e
Adrian <adrianj66@interia.pl>
2020-05-20 12:09:48 +0200

    implement services interfaces

---
+++ petclinic-data/src/main/java/springframework/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package springframework.sfgpetclinic.services;
+
+import springframework.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService
+{
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
