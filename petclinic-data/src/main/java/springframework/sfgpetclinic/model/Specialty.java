1317ea045fbb940a7ae0ee46a90e6901b425719e
Adrian <adrianj66@interia.pl>
2020-06-25 14:48:11 +0200

    completed JPA mapping

--- petclinic-data/src/main/java/springframework/sfgpetclinic/model/Specialty.java
+++ petclinic-data/src/main/java/springframework/sfgpetclinic/model/Specialty.java
@@ -2,8 +2,15 @@ package springframework.sfgpetclinic.model;
 
 import org.springframework.stereotype.Service;
 
-@Service
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
